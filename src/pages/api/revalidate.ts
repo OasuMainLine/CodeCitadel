945cdae6b811627beafba3275435c756712d12f0
OasuMainLine <andressu23@gmail.com>
2023-05-15 23:15:28 +0200

    Revalidation fix - 1.0.6

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -31,7 +31,6 @@ export default async function handler(
 		await res.revalidate("/blog");
 		await res.revalidate("/");
 
-		await writeSearches();
 		return res.json({ revalidated: true });
 	} catch (err) {
 		// If there was an error, Next.js will continue
